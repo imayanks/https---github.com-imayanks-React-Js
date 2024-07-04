0f2730153f9bb577a1a2e301742b76ce347dee8b
Mayank Shashank <115707644+imayanks@users.noreply.github.com>
2024-07-04 18:59:24 +0530

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+const jsxHeading=<h1 id="head"> Namaste React From Jsx 🚀</h1>
+
+console.log(head);
+
+const root =ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(jsxHeading);
